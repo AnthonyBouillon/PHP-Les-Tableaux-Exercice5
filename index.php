036a590c59e08329dfc8a48a8dcec74b926119ef
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 13:59:24 +0100

    init commit from bash for PHP-Les-Tableaux-Exercice5

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les tableaux Exercice 5</title>
+  </head>
+  <body>
+    <?php
+      $department = array('02' => 'Aisne', '59' => 'Nord', '60' => 'Oise', '62' => 'Pas-de-Calais', '80' => 'Somme');
+      print_r($department);
+     ?>
+  </body>
+</html>
